eb5bdeeb190e712bf77f2cb287bf80fee3475c8f
mt891 <torres.m@opendevpro.com>
2025-08-08 17:19:11 -0300

    mejorando algoritmo de calculo

--- src/app/utils/calcularPresupuesto.ts
+++ src/app/utils/calcularPresupuesto.ts
@@ -5,27 +5,54 @@ interface Iitem {
   price: number;
 }
 
+
 interface presupuestoReturn {
     items: Iitem[];
     presupuestoRestante: number;
 }
 
+
 export const calcularMejorPresupuesto = (items: Iitem[], presupuesto: number): presupuestoReturn => {
-  let presupuestoRestante = presupuesto;
-  const itemsSeleccionados: Iitem[] = [];
 
-  const itemsOrdenados = [...items].sort((a, b) => b.price - a.price);
+  presupuesto = parseInt(presupuesto.toString(), 10);
+  const n = items.length;
+  const dp: number[][] = Array(n + 1).fill(null).map(() => Array(presupuesto + 1).fill(0));
+  for (let i = 1; i <= n; i++) {
+    const currentItem = items[i - 1];
+    for (let j = 1; j <= presupuesto; j++) {
+      if (currentItem.price <= j) {
 
-  for (const item of itemsOrdenados) {
-    if (item.price <= presupuestoRestante) {
-      itemsSeleccionados.push(item);
-      presupuestoRestante -= item.price;
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - currentItem.price] + currentItem.price);
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
     }
   }
+
+
+
+  console.log('lkasd',dp);
+  const itemsSeleccionados: Iitem[] = [];
+  let res = dp[n][presupuesto];
+  let w = presupuesto;
+console.log(res)
+  for (let i = n; i > 0 && res > 0; i--) {
+
+    if (res !== dp[i - 1][w]) {
+      const selectedItem = items[i - 1];
+      itemsSeleccionados.push(selectedItem);
+      res -= selectedItem.price;
+      w -= selectedItem.price;
+    }
+  }
+
+  itemsSeleccionados.reverse();
+
+  const presupuestoTotalSeleccionado = itemsSeleccionados.reduce((sum, item) => sum + item.price, 0);
+  const presupuestoRestante = presupuesto - presupuestoTotalSeleccionado;
 
   return {
     items: itemsSeleccionados,
     presupuestoRestante
   };
 };
-
